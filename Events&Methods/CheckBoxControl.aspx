8b599e15e8dd445dd1413d51f5814e03d3199a52
arshid <arshid.ahmad1@gmail.com>
2024-07-15 11:12:37 +0530

    CheckBox Controls

---
+++ Events&Methods/CheckBoxControl.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="CheckBoxControl.aspx.cs" Inherits="Events_Methods.CheckBoxControl" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div style="font-family:Arial">
+            <fieldset>
+                <legend><b>Education</b> </legend>
+                    <asp:CheckBox ID="GraduateCheckBox" Text="Graduate" runat="server" />
+                    <asp:CheckBox ID="PostGraduateCheckBox" Text="PostGraduate" runat="server" />
+                    <asp:CheckBox ID="PHDCheckbox" Text="PHD" runat="server" />
+
+               </fieldset>
+            <br /><br />
+            <asp:Button ID="Button1" runat="server" Text="Button" OnClick="Button1_Click" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ Events&Methods/CheckBoxControl.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Text;
+
+namespace Events_Methods
+{
+    public partial class CheckBoxControl : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            StringBuilder sbUserChoices=new StringBuilder();
+            if (GraduateCheckBox.Checked )
+            {
+                sbUserChoices.Append(GraduateCheckBox.Text);
+            }
+            if (PostGraduateCheckBox.Checked)
+            {
+                sbUserChoices.Append(PostGraduateCheckBox.Text);
+            }
+            if (PHDCheckbox.Checked) 
+            { 
+            sbUserChoices.Append(PHDCheckbox.Text);
+            }
+            Response.Write("Your Selection" + sbUserChoices.ToString());
+        }
+    }
+}
